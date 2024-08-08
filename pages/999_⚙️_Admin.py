c59a8c65cca5eeb6660af2abe5cd84d4ab0a9d33
Beck Orion <beckorion@gmail.com>
2024-08-08 12:19:53 -0700

    added a server update button in the admin pane and enabled the admin portal

--- pages/999_⚙️_Admin.py
+++ pages/999_⚙️_Admin.py
@@ -1,14 +1,67 @@
+# Import necessary libraries
 import streamlit as st
 import asset_director
-import streamlit_lottie
-src = asset_director.Asset("Admin", 999)
-loading = src.under_construction()
+import os
 
+# Configure assets
+src = asset_director.Asset("Admin", 999)
+# loading = src.under_construction()
+
+# Configure the Streamlit app
 st.set_page_config(page_title=src.tab_title(),
     page_icon=src.favicon(),
     layout="centered",
     initial_sidebar_state="expanded")
 
-st.markdown(loading[0], unsafe_allow_html=True)
+# Set up session state
+if "auth" not in st.session_state:
+    st.session_state["auth"] = False
 
-streamlit_lottie.st_lottie(loading[1], speed=1, height=300)
+if "sudo_mode" not in st.session_state: # Sudo mode is a checkbox on login to allow destructive actions from the web interface
+    st.session_state["sudo_mode"] = False # Off by default
+
+if st.session_state["sudo_mode"] is True: # Sudo mode is on
+    st.session_state["block_destructive_actions"] = False # Will NOT disable destructive action buttons
+else: # Sudo mode is off
+    st.session_state["block_destructive_actions"] = True # Will disable destructive action buttons
+
+## Page content
+st.title("Admin Portal")
+
+# Sign-in page
+if st.session_state["auth"] is False:
+    with st.form(key="login_form"):
+        username = st.text_input("Username", key="username")
+        password = st.text_input("Password", type="password", key="password")
+
+        sudo_mode = st.checkbox("Sudo Mode", key="sudo_mode_checkbox", value=False)
+
+        login_button = st.form_submit_button(label="Login")
+        st.markdown("*You might have to click the password field to get autofill to show up*")
+
+        if login_button:
+            if username == st.secrets["ADMIN_USERNAME"] and password == st.secrets["ADMIN_PASSWORD"]:
+                st.session_state["auth"] = True
+                if sudo_mode is True:
+                    st.session_state["sudo_mode"] = True
+                st.rerun() # Rerun the script to display the admin portal
+            else:
+                st.error("Incorrect username or password")
+
+# Admin portal
+if st.session_state["auth"] is True:
+    # Header
+    if st.session_state["sudo_mode"] is True:
+        st.warning("Sudo Mode Enabled - All destructive actions are disabled - Be careful!")
+    else:
+        st.text("Standard mode")
+    st.divider()
+
+
+    # Update server button
+    if st.button("Update Server", key="update_server", disabled=st.session_state["block_destructive_actions"]):
+        st.toast("Updating server...")
+        st.toast("Make sure to refresh in a minute!")
+        os.system("~/Documents/update_becks_website.sh")
+    st.markdown("""<p style='color:orange;'>This button will only work on the server, not in a development environment.</p>""", unsafe_allow_html=True)
+    st.markdown("""<p style='color:red;'>If you've updated, added, or changed any secrets, you will have to add those manually.</p>""", unsafe_allow_html=True)
